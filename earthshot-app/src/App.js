00f5674238a925a6498a9ccd539865a8548c617e
oAllenDing <allen.ding@earthshot.ca>
2024-01-04 12:38:09 -0800

    add more containers using grid and assigned bgcolours

--- earthshot-app/src/App.js
+++ earthshot-app/src/App.js
@@ -20,9 +20,33 @@ function App() {
           900M = malnutrition.
         </Typography>
       </Box>
-      {/* <Grid item>
-        test
-      </Grid> */}
+      <Grid
+        container spacing={2}
+        direction="row"
+        justify="space-evenly"
+        alignItems="center">
+        <Grid item>
+          <h1 style={{ backgroundColor: '#9ACD32' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: '#FF7F7F' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: 'lightblue' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: '#3B3C04' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: '#FFAE42' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: '#90EE90' }}>Testing</h1>
+        </Grid>
+        <Grid item>
+          <h1 style={{ backgroundColor: '#CBC3E3' }}>Testing</h1>
+        </Grid>
+      </Grid>
     </Container >
   );
 }
