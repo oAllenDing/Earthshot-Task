338164021185d4e4201ca1eeee64dea558a56a3a
oAllenDing <allen.ding@earthshot.ca>
2024-01-05 17:33:30 -0800

    refactor and moved css into index.js

--- earthshot-app/src/App.js
+++ earthshot-app/src/App.js
@@ -1,53 +1,19 @@
 import './App.css';
-import { Box, Container, Typography, Grid } from '@mui/material';
-
-
+import { Grid } from '@mui/material';
 
 
 function App() {
   return (
-    <Container maxWidth="xs" >
-      <Box
-        sx={{
-          pt: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: 'center',
-          gap: 4,
-          bgcolor: 'black',
-        }}>
-        <Typography variant='h1' sx={{ my: 4, mb: 7, textAlign: 'center', color: 'primary.main' }}>
-          900M = malnutrition.
-        </Typography>
-      </Box>
+    <div className='App'>
       <Grid
-        container spacing={2}
-        direction="row"
-        justify="space-evenly"
-        alignItems="center">
-        <Grid item>
-          <h1 style={{ backgroundColor: '#9ACD32' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: '#FF7F7F' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: 'lightblue' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: '#3B3C04' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: '#FFAE42' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: '#90EE90' }}>Testing</h1>
-        </Grid>
-        <Grid item>
-          <h1 style={{ backgroundColor: '#CBC3E3' }}>Testing</h1>
-        </Grid>
+        spacing={5}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
       </Grid>
-    </Container >
+    </div>
   );
 }
 
--- earthshot-app/src/index.js
+++ earthshot-app/src/index.js
@@ -1,39 +1,162 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider, createTheme } from "@mui/material";
+import { ThemeProvider, createTheme, Container, Grid, Box, Typography } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FFFFFF",
-    },
-    secondary: {
-      main: "#2e74c9",
-    },
+const content = [
+  {
+    color: '#9B7A01',
+    boxNumber: '1',
+    titleText: '30% = Food & ag.',
   },
-  typography: {
-    h1: {
-      fontSize: "2em",
-      fontWeight: 600,
-    },
-    h2: {
-      fontSize: "1.75em",
-      fontWeight: 600,
-    },
-    h3: {
-      fontSize: "1.5em",
-      fontWeight: 600,
-    },
+  {
+    color: '#DA5B38',
+    boxNumber: '2',
+    titleText: '10% = healthcare.',
   },
-});
+  {
+    color: '#58B9EA',
+    boxNumber: '3',
+    titleText: '25% = water.',
+  },
+  {
+    color: '#373E02',
+    boxNumber: '4',
+    titleText: '30% = energy.',
+  },
+  {
+    color: '#FFAB0F',
+    boxNumber: '5',
+    titleText: '3% = housing.',
+  },
+  {
+    color: '#4CBB17',
+    boxNumber: '6',
+    titleText: '5% = environment.',
+  },
+  {
+    color: '#9F00FF',
+    boxNumber: '7',
+    titleText: '2% = economic.',
+  },
+];
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+<div
+    style={{
+      backgroundColor: "#1d1d1d",
+      height: "95vh",
+      width: "95vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      flexDirection: "column",
+      color: "white",
+      padding: "8px",
+    }}
+  >
+    <div
+      style={{
+        width: "25%",
+        padding: "8px",
+        borderRadius: "12px",
+        backgroundColor: "black",
+      }}
+    >
+      <div>Bad Outcome #1</div>
+      <Typography variant="h6">900M = malnutrition.</Typography>
+      <div style={{ marginBottom: "8px" }}>Globally, 1 in 3 ≠ safe drinking water, mostly ≠ LDC's. </div>
+      <div
+        style={{
+          borderRadius: "12px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              height: "8px",
+              width: "40px",
+              backgroundColor: "green",
+              borderRadius: "15%",
+              marginRight: "4px",
+            }}
+          />
+          <div
+            style={{
+              height: "8px",
+              width: "40px",
+              backgroundColor: "green",
+              borderRadius: "15%",
+              marginRight: "4px",
+            }}
+          />
+          <div
+            style={{
+              height: "8px",
+              width: "40px",
+              backgroundColor: "green",
+              borderRadius: "15%",
+              marginRight: "4px",
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ marginBottom: "8px" }}>SUMMARY</div>
+      <div style={{ marginBottom: "8px" }}>...</div>
+    </div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+      }}
+    >
+      {content.map((item) => (
+        <div
+          style={{
+            backgroundColor: item.color,
+            borderRadius: "12px",
+            padding: "8px",
+          }}
+        >
+          <div style={{ marginBottom: "8px" }}> Bucket #{item.boxNumber}</div>
+          <div style={{ marginBottom: "8px" }}>{item.titleText}</div>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                height: "8px",
+                width: "40px",
+                backgroundColor: "green",
+                borderRadius: "15%",
+                marginRight: "4px",
+              }}
+            />
+            <div
+              style={{
+                height: "8px",
+                width: "40px",
+                backgroundColor: "green",
+                borderRadius: "15%",
+                marginRight: "4px",
+              }}
+            />
+            <div
+              style={{
+                height: "8px",
+                width: "40px",
+                backgroundColor: "green",
+                borderRadius: "15%",
+                marginRight: "4px",
+              }}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>,
+  // rootElement
 );
 
